f4d44e00c64d9463947326000099450bc7655923
Dax Wann <daxwann@gmail.com>
2020-10-08 10:35:47 -0700

    add prop-types and check-prop-types dependencies

--- src/component/headline/index.js
+++ src/component/headline/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Headline extends Component {
   render() {
@@ -9,16 +10,28 @@ class Headline extends Component {
     }
 
     return (
-      <div class="headline" data-test="headline">
-        <h1 class="headline--header" data-test="headline--header">
+      <div className="headline" data-test="headline">
+        <h1 className="headline--header" data-test="headline--header">
           {header}
         </h1>
-        <p class="headline--desc" data-test="headline--desc">
+        <p className="headline--desc" data-test="headline--desc">
           {desc}
         </p>
       </div>
     )
   }
+}
+
+Headline.propTypes = {
+  header: PropTypes.string,
+  desc: PropTypes.string,
+  tempArr: PropTypes.arrayOf(PropTypes.shape({
+    fName: PropTypes.string,
+    desc: PropTypes.string,
+    email: PropTypes.string,
+    age: PropTypes.number,
+    onlineStatus: PropTypes.bool
+  }))
 }
 
 export default Headline;
