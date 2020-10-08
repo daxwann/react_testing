4bc67ecc4a3cd94567ecd65a0846344aa80f946d
Dax Wann <daxwann@gmail.com>
2020-10-08 10:46:15 -0700

    add test for headline proptypes

--- src/component/headline/headline.spec.js
+++ src/component/headline/headline.spec.js
@@ -2,13 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Headline from './index';
 
-import { findByTestAttr } from '../../../utils';
+import { findByTestAttr, checkProps } from '../../../utils';
 
 describe("Headline Component", () => {
   const setUp = (props={}) => {
     const component = shallow(<Headline {...props}/>);
     return component;
   }
+
+  describe("Checking proptypes", () => {
+    it("should not throw a warning", () => {
+      const expectedProps = {
+        header: "Test Header",
+        desc: "Test Desc",
+        tempArr: [{
+          fName: "Test fName",
+          lName: "Test lName",
+          email: "test@email.com",
+          age: 23,
+          onlineStatus: false
+        }]
+      }
+
+      const propsErr = checkProps(Headline, expectedProps);
+      expect(propsErr).toBeUndefined();
+    })
+  })
 
   describe("Have props header", () => {
     let component;
